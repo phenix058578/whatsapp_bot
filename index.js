c60315bd4369c80465f97edfd9a4dfe4fcbc2146
phenix058578 <cosikylian@gmail.com>
2026-05-06 14:07:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import Groq from "groq-sdk";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState("auth");
+   const { state, saveCreds } = await useMultiFileAuthState("auth_info");
+
 
     const sock = makeWASocket({
         auth: state,
